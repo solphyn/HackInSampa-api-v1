02ad4982e1adb77508fb0689be605c89eff59f6d
Diego Bispo <dbispo@gmail.com>
2020-12-19 01:35:21 -0300

    iniciando projeto
    iniciando estrutura com HapiJs

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await server.start();
+    console.log('Servidor online! %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
